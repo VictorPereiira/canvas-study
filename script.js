893f6fc80cff06027fe124362d2abbc5c21ca9d1
victor <victor.psa02@gmail.com>
2021-05-12 18:07:24 -0300

    feat: start renderScreen

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const screen = document.getElementById('screen')
 const context = screen.getContext('2d');
 
-const element = {
-    'color': 'red',
-    'positionX': 0,
-    'positionY': 0,
-    'width': 250,
-    'height': 250
+const game = {
+    players: {
+        'player1': { x: 1, y: 1 },
+        'player2': { x: 9, y: 9 }
+    },
+    fruits: {
+        'fruit1': { x: 3, y: 1 }
+    }
 }
 
-context.fillStyle = element.color
-context.fillRect(element.positionX, element.positionY, element.width, element.height)
+renderScreen()
+
+function renderScreen() {
+    context.fillStyle = 'white'
+    context.fillRect(0, 0, 10, 10)
+
+    for (playerId in game.players) {
+        const player = game.players[playerId]
+        context.fillStyle = 'black'
+        context.fillRect(player.x, player.y, 1, 1)
+    }
+    for (fruitId in game.fruits) {
+        const fruit = game.fruits[fruitId]
+        context.fillStyle = 'green'
+        context.fillRect(fruit.x, fruit.y, 1, 1)
+    }
+
+    requestAnimationFrame(renderScreen)
+}
+
